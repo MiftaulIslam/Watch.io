ab37f9e49c17f7863f3abab3872bebbbaae563ea
Miftaul_Islam_Ariyan <124516497+MiftaulIslam@users.noreply.github.com>
2023-09-16 17:53:05 +0600

    Add files via upload

---
+++ componentsIndex.js
@@ -0,0 +1,12 @@
+export {default as Header} from './header/Header'
+
+export {default as Footer} from './footer/Footer'
+export {default as Img} from './lazyload/img'
+export {default as Contentwrapper} from './wrapper/ContentWrapper'
+export {default as Switchtimetabs} from './switchtimeTabs/Switchtimetabs'
+export {default as Switchtypetabs} from './switchtype/Switchtypetabs'
+export {default as Trendingmovies} from './trending movies/Trendingmovies'
+export {default as Cast} from './cast/Cast'
+export {default as Moviedetailscontent} from './movie details content/Moviedetailscontent'
+export {default as Recomendation} from './recomendation/Recomendation'
+
